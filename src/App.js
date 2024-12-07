827426b58bcc690da88337708368d118b7c242b0
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-07 17:07:09 +0000

    Added react-bbotstrap, fontawsome, favicon and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+//import Button from "react-bootstrap/Button";
+import NavBar from "./components/NavBar";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <NavBar />
     </div>
   );
 }
