006dbd9280d5eeb1c3d5d3970f8f0a1a3c5c36f1
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-09 14:25:41 +0000

    Save and commit

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -32,7 +32,7 @@ function SignInForm() {
     event.preventDefault();
 
     try {
-      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
