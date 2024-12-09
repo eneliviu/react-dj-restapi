0a1b49a1b1a0b63e8d0dbedd1892d5535721a734
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-09 16:30:37 +0000

    Save and commit

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 // the unique URL of the deployed project
-axios.defaults.baseURL = 'https://react-dj-restapi-eb6a7149ec97.herokuapp.com';
+axios.defaults.baseURL = 'https://dj-drf-api-763634fa56e5.herokuapp.com';
 // data format the API will be expecting
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // To avoid any CORS errors when sending cookies
 axios.defaults.withCredentials = true;
+
+
+// axios inteceptors to refresh the tokens
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
